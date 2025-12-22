08cda8be0f42ea0b0bf3be484e9969791af3b1e5
aybinv7 <aybinventor7@gmail.com>
2025-12-22 16:29:29 +0100

    Auto-deploy assets: 2025-12-22T15:29:26.297Z

--- assets/GroupDetail-0U7w4hbQ.js
+++
@@ -1,1 +0,0 @@
-import"./f7-core-qtncDiL6.js";import"./f7-vue-UbQyz5qn.js";import"./vendor-Dj-Vf5uX.js";import"./shared-DMHKFyVv.js";import{a as e}from"./app-modules-CGCPaG24.js";import"./app-modules-C_Mg4SWy.js";export{e as default};
