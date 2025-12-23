14cea7c484f6541ab4016403584344d919e424c1
aybinv7 <aybinventor7@gmail.com>
2025-12-23 22:22:25 +0100

    Auto-deploy assets: 2025-12-23T21:22:23.411Z

---
+++ assets/GroupDetailExtra-DIf-m84Z.js
@@ -0,0 +1,1 @@
+import"./f7-core-qtncDiL6.js";import"./f7-vue-RG7PfwlE.js";import"./vendor-CVuyWred.js";import"./shared-DrX8Jij_.js";import{i as e}from"./app-modules-58RM4HxX.js";export{e as default};
