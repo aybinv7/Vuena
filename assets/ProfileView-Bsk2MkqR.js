ce2d1591f0c869494624d4ea58cf7771c3769b8a
aybinv7 <aybinventor7@gmail.com>
2026-01-10 15:27:07 +0100

    Auto-deploy assets: 2026-01-10T14:27:02.173Z

---
+++ assets/ProfileView-Bsk2MkqR.js
@@ -0,0 +1,1 @@
+import"./f7-core-qtncDiL6.js";import"./f7-vue-UbQyz5qn.js";import"./vendor-Dj-Vf5uX.js";import"./shared-ClIAYA7z.js";import{t as e}from"./app-modules-GiH10mjh.js";import"./app-modules-TNfrKTmP.js";export{e as default};
